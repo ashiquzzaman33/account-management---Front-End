ae3bde2ca8b591ea02ef1bf80a046395c7d42419
Mahmud Raju <mahmudkuet11@gmail.com>
2015-05-22 18:13:13 +0600

    first step validation added

--- src/account/management/controller/NewVoucherController.java
+++ src/account/management/controller/NewVoucherController.java
@@ -7,8 +7,20 @@ package account.management.controller;
 
 import account.management.model.Account;
 import account.management.model.Location;
+import account.management.model.MetaData;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import java.net.URL;
+import java.util.Collection;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -20,7 +32,9 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import org.json.JSONArray;
 
 /**
  * FXML Controller class
@@ -49,10 +63,50 @@ public class NewVoucherController implements Initializable {
     @FXML
     private Button button_delete_row;
     
+    private Collection<Location> locations;
+    
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        select_voucher_type.getItems().addAll("JV","CV","PV","SV");
         
+        /*
+        *   init locations in select_location combobox
+        */
+        locations = FXCollections.observableArrayList();
+        new Thread(new Runnable(){
+
+            @Override
+            public void run() {
+                try {
+                    HttpResponse<JsonNode> response = Unirest.get(MetaData.baseUrl + "get/locations").asJson();
+                    JSONArray location_array = response.getBody().getArray();
+                    for(int i=0; i<location_array.length()-1; i++){
+                        String name = location_array.getJSONObject(i).getString("name");
+                        String details = location_array.getJSONObject(i).getString("details");
+                        int id = location_array.getJSONObject(i).getInt("id");
+                        locations.add(new Location(id,name,details));
+                    }
+                    select_location.getItems().addAll(locations);
+                    
+                } catch (UnirestException ex) {
+                    System.out.println("exception in UNIREST");
+                }
+            }
+            
+        }).start();
+        
+        this.button_submit.setDisable(true);
+        
+        field_container.setOnKeyReleased(new EventHandler<KeyEvent>(){
+
+            @Override
+            public void handle(KeyEvent event) {
+                validateFields();
+                
+            }
+            
+        });
         
         
     }    
@@ -94,9 +148,12 @@ public class NewVoucherController implements Initializable {
             @Override
             public void handle(MouseEvent event) {
                 field_container.getChildren().removeAll(row);
+                validateFields();
             }
             
         });
+        
+        validateFields();
         
     }
 
@@ -107,6 +164,61 @@ public class NewVoucherController implements Initializable {
     @FXML
     private void onDeleteRowButtonClick(MouseEvent event) {
         field_container.getChildren().removeAll(field_row);
+        validateFields();
+    }
+    
+    private void validateFields(){
+        
+        int dr_counter = 0, cr_counter = 0;
+        float total_dr = 0f, total_cr = 0f;
+        
+        for(int i=0; i<field_container.getChildren().size(); i++){
+            HBox row = (HBox) field_container.getChildren().get(i);
+            TextField dr = (TextField) row.getChildren().get(1);
+            TextField cr = (TextField) row.getChildren().get(2);
+            
+            if(dr.getText().equals("") && cr.getText().equals("")){
+                this.button_submit.setDisable(true);
+                return;
+            }
+            if(!dr.getText().equals("") && !cr.getText().equals("")){
+                this.button_submit.setDisable(true);
+                return;
+            }
+            /*
+            *   regular expression check
+            */
+            Pattern pattern = Pattern.compile("^[0-9]*\\.?[0-9]+$");
+            Matcher match = pattern.matcher(dr.getText().toString());
+            if(!dr.getText().equals("") && !match.find()){
+                this.button_submit.setDisable(true);
+                return;
+            }
+            match = pattern.matcher(cr.getText().toString());
+            if(!cr.getText().equals("") && !match.find()){
+                this.button_submit.setDisable(true);
+                return;
+            }
+            
+            
+            if(!dr.getText().equals("")){
+                dr_counter++;
+                total_dr += Float.parseFloat(dr.getText().toString());
+            }
+            if(!cr.getText().equals("")){
+                cr_counter++;
+                total_cr += Float.parseFloat(cr.getText().toString());
+            }
+                
+        }
+        
+        if((dr_counter > 1 && cr_counter > 1) || dr_counter ==0 || cr_counter == 0 || (total_dr != total_cr)){
+            this.button_submit.setDisable(true);
+            return;
+        }
+        
+        this.button_submit.setDisable(false);
+        
     }
     
 }
